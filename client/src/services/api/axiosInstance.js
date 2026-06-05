da99377e2b2f1708bd5045a102524779a7d2693e
vladlensweetbits <vladt@sweetbits.io>
2026-06-05 20:12:12 +0300

    add connection from render

--- client/src/services/api/axiosInstance.js
+++ client/src/services/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://trvd-2026-401th-tymoshenko-labs.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
